62b97feef6bc564b0b9ac9533a5425cbaf5a332b
seanng <shonum@gmail.com>
2019-04-23 16:24:13 +0800

    INITIAL COMMIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import QRCode from 'qrcode.react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('')
+  const [qrCode, setQrCode] = useState('')
+
+  useEffect(() => {
+    setInput('')
+  }, [qrCode])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <QRCode value={qrCode} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Current QR Code Value: {qrCode}
+          <br />
+          <br />
+          <input 
+            type="text" 
+            value={input} 
+            onChange={e => setInput(e.target.value)}
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setQrCode(input)}>
+          SAVE
+        </button>
       </header>
     </div>
   );
